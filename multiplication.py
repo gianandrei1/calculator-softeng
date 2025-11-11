dc609791ec555d3686294249254dbbfd7ef1e954
20210022354 <20210022354@my.xu.edu.ph>
2025-11-11 14:41:57 +0800

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,30 @@
+class calculator:
+        whileChecker = True
+	def multiplication(x,y):
+		result = x*y
+		print(result)
+
+
+	while whileChecker == true:
+		choice = int(input("1 - addition, 2 - subtraction, 3 - multiplication, 4 - division, 5 - exit "))
+		if choice == 1:
+			x = int(input("first value: "))
+			y = int(input("second value: "))
+			addition(x,y)
+		elif choice == 2:
+			x = int(input("first value: "))
+			y = int(input("second value: "))
+			subtraction(x,y)
+		elif choice == 3:
+			x = int(input("first value: "))
+			y = int(input("second value: "))
+			multiplication(x,y)
+		elif choice == 4:
+			x = int(input("first value: "))
+			y = int(input("second value: "))
+			division(x,y)
+		elif choice == 5:
+			whileChecker = False
+		else:
+                	print("wrong input")
+	
